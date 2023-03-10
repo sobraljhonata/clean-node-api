c1378e597f5dbd41b5861ac41f08f06f0945af8b
Jhonata Sobral <sobral.jhonata@gmail.com>
2023-03-10 08:02:48 -0400

    feat: ensure SignUpController returns 400 if an invalid email is privided

--- src/presentation/protocols/controller.ts
+++ src/presentation/protocols/controller.ts
@@ -1,5 +1,5 @@
 import { HttpRequest, HttpResponse } from './http'
 
 export interface Controller {
-  handle: (httpRequest: HttpRequest) => HttpResponse | any
+  handle: (httpRequest: HttpRequest) => HttpResponse
 }
