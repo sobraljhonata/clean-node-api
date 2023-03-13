a7d142c959c9d94aee20091bbef3297499fd4747
Jhonata Sobral <sobral.jhonata@gmail.com>
2023-03-13 16:15:41 -0400

    refactor: move mapping to MongoHelper

--- src/ infra/db/mongodb/account-repository/account.ts
+++ src/ infra/db/mongodb/account-repository/account.ts
@@ -3,23 +3,12 @@ import { AccountModel } from '../../../../domain/models/account'
 import { AddAccountModel } from '../../../../domain/usecases/add-account'
 import { MongoHelper } from '../helpers/mongo-helper'
 
-interface AccountModelMongo {
-  _id: string
-  name: string
-  email: string
-  password: string
-}
-
 export class AccountMongoRepository implements AddAccountRepository {
   async add (accountData: AddAccountModel): Promise<AccountModel> {
     const accountColletion = MongoHelper.getCollection('accounts')
     const result = await accountColletion.insertOne(accountData)
-    const account = await accountColletion.findOne<AccountModelMongo>({ _id: result.insertedId.toHexString })
+    const account = await accountColletion.findOne<any>({ _id: result.insertedId.toHexString })
 
-    if (account === null) {
-      throw new Error()
-    }
-    const { _id, ...accountWithoutId } = account
-    return Object.assign({}, accountWithoutId, { id: _id })
+    return MongoHelper.map(account)
   }
 }
--- src/ infra/db/mongodb/helpers/mongo-helper.ts
+++ src/ infra/db/mongodb/helpers/mongo-helper.ts
@@ -4,7 +4,7 @@ export const MongoHelper = {
   client: (null as unknown) as MongoClient,
 
   async connect (uri: string): Promise<void> {
-    this.client = await MongoClient.connect(process.env.MONGO_URL as string)
+    this.client = await MongoClient.connect(uri)
   },
 
   async disconnect (): Promise<void> {
@@ -13,5 +13,10 @@ export const MongoHelper = {
 
   getCollection (name: string): Collection {
     return this.client.db().collection(name)
+  },
+
+  map (collection: any): any {
+    const { _id, ...collectionWithoutId } = collection
+    return Object.assign({}, collectionWithoutId, { id: _id })
   }
 }
